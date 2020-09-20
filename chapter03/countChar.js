9cbf1b6b71a647fc8a32d8bbf1ed2d74786a5ca8
Mahammad Muneer N B <muneer@robosoftin.com>
2020-09-20 12:25:30 +0530

    Correcting the countChar program

--- chapter03/countChar.js
+++ chapter03/countChar.js
@@ -3,14 +3,14 @@ it takes a second argument that indicates the character that is to be counted
 (rather than counting only uppercase “B” characters). */
 "use strict";
 
-function countBb(s) {
+function countChar(string, char) {
     let count = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === "B" || s[i] === "b") {
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === char) {
             count++;
         }
     }
     return count;
 }
 
-console.log(countBb("BabaBa"));
+console.log(countChar("BabaBa", "a"));
